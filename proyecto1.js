0ce1979f0b5a7b0aaff61f9f9c45ed91b4590883
zalo1490 <145482713+zalo1490@users.noreply.github.com>
2023-11-02 12:45:35 +0100

    Update proyecto1.js

--- proyecto1.js
+++ proyecto1.js
@@ -1,16 +1,1 @@
-import { neonCursor} from
-'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.csn.min.js'
 
-neonCursor ({
-    el: document.getElementById('app'),
-    shaderPoints: 16,
-    cuverPoints: 80,
-    curveLerp: 0.5,
-    raduis1: 5,
-    radius2: 30,
-    velocityIreshold: 10,
-    sleepRadiusX: 100,
-    sleepRadiusY: 100,
-    sleepTimeCoefX: 0.0025,
-    sleepTimeCoefY: 0.0025
-})
